f475ac39c9d8ce86bb99e96f2d599793342fd13a
Tiago <tgdsilva@gmail.com>
2014-12-11 13:34:49 +0000

    Cenas

---
+++ Pilot/src/log4Pack/Log4jHelloWorld.java
@@ -0,0 +1,31 @@
+package log4Pack;
+
+import org.apache.log4j.Logger;
+
+public class Log4jHelloWorld {
+	
+	static Logger first = Logger.getLogger(WriteFirst.class);
+//	static Logger second = Logger.getLogger(WriteSecond.class);
+	
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+	
+//		BasicConfigurator.configure();
+//		log.debug("This is a message");
+		
+		//WriteFirst first = new WriteFirst("WriteFirst");
+		//WriteSecond second = new WriteSecond("WriteSecond");
+		
+//		first.setMessage("Test Debug", 'D');
+//		second.setMessage("Test Report", 'D');
+		
+		
+//		BasicConfigurator.configure();
+//		first.debug("This is a message");
+//		second.debug("This is a message");
+		
+		//first.getLogger("dasda");
+		
+		
+	}
+}
---
+++ Pilot/src/log4Pack/WriteSecond.java
@@ -0,0 +1,67 @@
+package log4Pack;
+
+import org.apache.log4j.Logger;
+
+public class WriteSecond extends File {
+
+	private String logName;
+
+	public WriteSecond(String logname) {
+		this.logName = logname;
+	}
+
+	// Only for LOG Classes
+	public final Logger getReportsLog() {
+		return Logger.getLogger(getLogName());
+	}
+
+	// MESSAGE TO BE WRITTEN IN THE FILE
+	public void setMessage(String message, char type) {
+		// get the type of message that we want
+		// return the type
+		char tp = type;
+
+		
+		switch (tp) {
+
+		// Debug
+		case 'D':
+			getReportsLog().debug(message); //
+			break;
+
+		// Information
+		case 'I':
+			getReportsLog().info(message); //
+			break;
+
+		// Error
+		case 'E':
+			getReportsLog().error(message); //
+			break;
+
+		// Warning
+		case 'W':
+			getReportsLog().warn(message); //
+			break;
+
+		// Failure
+		case 'F':
+			getReportsLog().fatal(message); //
+			break;
+
+		default:
+			getReportsLog().info("Missing the type");
+			break;
+		}
+	}
+
+	// OTHERS
+	public String getLogName() {
+		return logName;
+	}
+
+	public void setLogName(String logName) {
+		this.logName = logName;
+	}
+
+}
